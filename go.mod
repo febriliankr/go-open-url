120ab22ede4d496a071d6d5ad58cffcce08a0703
Febrilian Kristiawan <febriliankr@gmail.com>
2021-10-18 19:55:28 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/febriliankr/go-open-url
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os/exec"
+	"runtime"
+)
+
+func open(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default:
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
+}
+
+func main() {
+	open("http://www.febrilian.com")
+
+}
